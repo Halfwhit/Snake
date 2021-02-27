884dc92637abe152d0e3d7d0abebe1311f488dc1
Halfwhit <Halfwhit@gmail.com>
2021-02-27 18:11:05 +0000

    Added a 2d camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build().add_plugins(DefaultPlugins).run();
+    App::build()
+        .add_startup_system(setup.system())
+    .add_plugins(DefaultPlugins).run();
+}
+
+fn setup(commands: &mut Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
